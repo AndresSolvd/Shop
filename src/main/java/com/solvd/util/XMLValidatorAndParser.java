26c68a109cd60045e20e0d28c8cd9897d84ecc83
andresmadeddie <abinary1010@gmail.com>
2023-06-07 19:59:48 -0400

    Added DOMParser, XML Validator, shop.xml and shop.xsd

--- src/main/java/com/solvd/util/XMLValidatorAndParser.java
+++ src/main/java/com/solvd/util/XMLValidatorAndParser.java
@@ -1,17 +1,10 @@
 package com.solvd.util;
 
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-
 import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
-import java.io.IOException;
 
 public class XMLValidatorAndParser {
 
@@ -22,7 +15,6 @@ public class XMLValidatorAndParser {
         try {
             boolean isValid = validateXMLAgainstXSD(xmlFilePath, xsdFilePath);
             if (isValid) {
-                Document document = parseXML(xmlFilePath);
                 // Process the validated and parsed XML document as needed
                 System.out.println("XML is valid and parsed successfully.");
             } else {
@@ -33,8 +25,7 @@ public class XMLValidatorAndParser {
         }
     }
 
-    public static boolean validateXMLAgainstXSD(String xmlFilePath, String xsdFilePath)
-            throws ParserConfigurationException, SAXException {
+    public static boolean validateXMLAgainstXSD(String xmlFilePath, String xsdFilePath) {
         try {
             SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
             Schema schema = schemaFactory.newSchema(new StreamSource(xsdFilePath));
@@ -45,12 +36,5 @@ public class XMLValidatorAndParser {
             e.printStackTrace();
             return false; // XML is not valid against the XSD
         }
-    }
-
-    public static Document parseXML(String xmlFilePath)
-            throws ParserConfigurationException, SAXException, IOException {
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        return documentBuilder.parse(xmlFilePath);
     }
 }
