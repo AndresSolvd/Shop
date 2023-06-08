6c2237171c0bddca5850163e86f87c31821bfbc4
andresmadeddie <abinary1010@gmail.com>
2023-06-08 14:37:01 -0400

    Separate validator and parser files. Use both new classes to validate and parse in the main method

--- src/main/java/com/solvd/Main.java
+++ src/main/java/com/solvd/Main.java
@@ -2,13 +2,13 @@ package com.solvd;
 
 import com.solvd.sql.model.*;
 import com.solvd.sql.services.*;
+import com.solvd.util.XmlParser;
+import com.solvd.util.XmlValidator;
 
 import java.sql.Date;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-
 
         // CATEGORY
         System.out.println("\n--- CATEGORY ---\n");
@@ -342,5 +342,7 @@ public class Main {
         // All
         System.out.println(productPromotionService.getAll());
 
+        // XML Validate and Parse
+        XmlParser.Parser(XmlValidator.Validator());
     }
 }
---
+++ src/main/java/com/solvd/util/XmlValidator.java
@@ -0,0 +1,41 @@
+package com.solvd.util;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+public class XmlValidator {
+    public static String Validator() {
+        String xmlFilePath = "src/main/resources/shop.xml";
+        String xsdFilePath = "src/main/resources/shop.xsd";
+
+        try {
+            boolean isValid = validateXMLAgainstXSD(xmlFilePath, xsdFilePath);
+            if (isValid) {
+                // Process the validated and parsed XML document as needed
+                System.out.println("XML is valid and parsed successfully.\n");
+                return xmlFilePath;
+            } else {
+                System.out.println("XML is not valid against the XSD.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "XML is not valid against the XSD";
+    }
+
+    public static boolean validateXMLAgainstXSD(String xmlFilePath, String xsdFilePath) {
+        try {
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(new StreamSource(xsdFilePath));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(xmlFilePath));
+            return true; // XML is valid against the XSD
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false; // XML is not valid against the XSD
+        }
+    }
+}
