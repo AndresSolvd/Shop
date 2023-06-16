c3265d731466dc4053e5d7df8b05557109f0781c
andresmadeddie <abinary1010@gmail.com>
2023-06-16 12:04:37 -0400

    Remove unused import statements

--- src/main/java/com/solvd/sql/jaxb/JAXBUtils.java
+++ src/main/java/com/solvd/sql/jaxb/JAXBUtils.java
@@ -8,11 +8,6 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
 
 public class JAXBUtils {
 
