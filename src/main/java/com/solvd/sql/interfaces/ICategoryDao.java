2b18703d0bc91321d3888376361e0b7bf5449b36
andresmadeddie <abinary1010@gmail.com>
2023-06-16 16:53:03 -0400

    update

--- src/main/java/com/solvd/sql/interfaces/ICategoryDao.java
+++ src/main/java/com/solvd/sql/interfaces/ICategoryDao.java
@@ -2,8 +2,6 @@ package com.solvd.sql.interfaces;
 
 import com.solvd.sql.model.Category;
 
-import java.sql.SQLException;
-
 public interface ICategoryDao extends IBaseDAO<Category> {
     Category getByName(String categoryName);
 }
