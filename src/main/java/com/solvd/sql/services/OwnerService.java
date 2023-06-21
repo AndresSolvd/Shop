88ee695ad928fde21219ac9c393b41771961d2af
andresmadeddie <abinary1010@gmail.com>
2023-06-21 15:47:43 -0400

    Create new method that give options to create a owner or to select from a list of owners when the input id is returns a null object Owner

--- src/main/java/com/solvd/sql/services/OwnerService.java
+++ src/main/java/com/solvd/sql/services/OwnerService.java
@@ -5,6 +5,7 @@ import com.solvd.sql.model.Owner;
 import com.solvd.sql.mybatis.OwnerDao;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class OwnerService implements IOwnerDao {
 
@@ -12,7 +13,19 @@ public class OwnerService implements IOwnerDao {
 
     @Override
     public void insert(Owner owner) {
-        ownerDao.insert(owner);
+
+        OwnerService ownerService = new OwnerService();
+
+        // Check if person exist
+        List<Owner> owners;
+        owners = ownerService.getAll();
+        if (!owners.contains(owner)) {
+            ownerDao.insert(owner);
+        } else {
+            createOwner();
+        }
+
+
     }
 
     @Override
@@ -32,11 +45,61 @@ public class OwnerService implements IOwnerDao {
 
     @Override
     public Owner getById(int id) {
+        if (ownerDao.getById(id) == null) {
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                System.out.println("The provided id does not match any owner on the Database\n" +
+                        "enter '1' to create a new owner\n" +
+                        "enter '2' to get a list of all owners in the Database\n" +
+                        "enter '3' to cancel this transaction");
+                String answer = scanner.nextLine();
+                switch (answer) {
+                    case "1":
+                        return new OwnerService().createOwner();
+                    case "2":
+                        System.out.println("List of all owners in Database\n" + new OwnerService().getAll()
+                                + "\n *** end *** \nInput new id: ");
+                        return getById(scanner.nextInt());
+                    case "3":
+                        return null; // Transaction canceled
+                    default:
+                        System.out.println("Invalid option");
+                        break;
+                }
+            }
+        }
         return ownerDao.getById(id);
     }
 
     @Override
     public Owner getOwnerByName(String name) {
         return ownerDao.getOwnerByName(name);
+    }
+
+    public Owner createOwner() {
+
+        Owner owner = new Owner();
+        OwnerService ownerService = new OwnerService();
+        PersonService personService = new PersonService();
+        Scanner scanner = new Scanner(System.in);
+
+        // input data
+        System.out.println("Enter Person id: ");
+        owner.setPerson(personService.getById(scanner.nextInt()));
+        scanner.close();
+
+        // Insert into Database
+        ownerService.insert(owner);
+
+        // Recover person to get the id
+        List<Owner> owners = ownerService.getAll();
+        for (Owner o : owners) {
+            if (o.getPerson().equals(owner.getPerson())) {
+                owner = o;
+            }
+        }
+        System.out.println("New Person created: \n" + owner + "\n   *****   \n");
+        return owner;
     }
 }
--- src/main/java/com/solvd/sql/services/PersonService.java
+++ src/main/java/com/solvd/sql/services/PersonService.java
@@ -24,8 +24,6 @@ public class PersonService implements IPersonDao {
         } else {
             createPerson();
         }
-
-
     }
 
     @Override
