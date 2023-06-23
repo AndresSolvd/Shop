c0ac31712c096165ee4fb4ba44a766021bc10a71
andres <agarcianavarro.laba@solvd.com>
2023-06-23 16:56:51 -0400

    Change methods for insert in the customer, owner,and staff services

--- src/main/java/com/solvd/sql/services/CustomerService.java
+++ src/main/java/com/solvd/sql/services/CustomerService.java
@@ -2,7 +2,6 @@ package com.solvd.sql.services;
 
 import com.solvd.sql.interfaces.ICustomerDao;
 import com.solvd.sql.model.Customer;
-import com.solvd.sql.model.Person;
 import com.solvd.sql.mybatis.CustomerDao;
 
 import java.util.List;
@@ -14,43 +13,10 @@ public class CustomerService implements ICustomerDao {
     @Override
     public void insert(Customer customer) {
         PersonService personService = new PersonService();
-        Person person = customer.getPerson();
-        List<Person> persons = personService.getAll();
-        List<Customer> customers = new CustomerService().getAll();
-        boolean exists = false;
 
-        // Search in database for the same person
-        for (Person p : persons) {
-            if (p.getPersonName().equals(person.getPersonName()) &&
-                    p.getLastName().equals(person.getLastName()) &&
-                    p.getPhone().equals(person.getPhone()) &&
-                    p.getAddress().equals(person.getAddress())) {
-                // if person exists add that person to the customer object
-                customer.setPerson(p);
-                exists = true;
-            }
-        }
-
-        // Check if customer with that person already exists
-        if (exists) {
-            for (Customer c : customers) {
-                if (c.getPerson().getPersonName().equals(customer.getPerson().getPersonName()) &&
-                        c.getPerson().getLastName().equals(customer.getPerson().getLastName()) &&
-                        c.getPerson().getPhone().equals(customer.getPerson().getPhone()) &&
-                        c.getPerson().getAddress().equals(customer.getPerson().getAddress())) {
-                    System.out.println(c.getPerson().getPersonName() + " " + c.getPerson().getLastName()
-                            + " already exist as customer.(customerId: " + c.getId() +
-                            " personId: " + c.getPerson().getId() + ")");
-                    return;
-                }
-            }
-        }
-
-        if (!exists) {
-            // if person does not exist create a new person in the database
+        if (personService.getById(customer.getPerson().getId()) == null) {
             personService.insert(customer.getPerson());
         }
-        // Insert customer in the Database
         customerDao.insert(customer);
     }
 
--- src/main/java/com/solvd/sql/services/OwnerService.java
+++ src/main/java/com/solvd/sql/services/OwnerService.java
@@ -2,7 +2,6 @@ package com.solvd.sql.services;
 
 import com.solvd.sql.interfaces.IOwnerDao;
 import com.solvd.sql.model.Owner;
-import com.solvd.sql.model.Person;
 import com.solvd.sql.mybatis.OwnerDao;
 
 import java.util.List;
@@ -14,43 +13,9 @@ public class OwnerService implements IOwnerDao {
     @Override
     public void insert(Owner owner) {
         PersonService personService = new PersonService();
-        Person person = owner.getPerson();
-        List<Person> persons = personService.getAll();
-        List<Owner> owners = new OwnerService().getAll();
-        boolean exists = false;
-
-        // Search in database for the same person
-        for (Person p : persons) {
-            if (p.getPersonName().equals(person.getPersonName()) &&
-                    p.getLastName().equals(person.getLastName()) &&
-                    p.getPhone().equals(person.getPhone()) &&
-                    p.getAddress().equals(person.getAddress())) {
-                // if person exists add that person to the owner object
-                owner.setPerson(p);
-                exists = true;
-            }
-        }
-
-        // Check if owner with that person already exists
-        if (exists) {
-            for (Owner o : owners) {
-                if (o.getPerson().getPersonName().equals(owner.getPerson().getPersonName()) &&
-                        o.getPerson().getLastName().equals(owner.getPerson().getLastName()) &&
-                        o.getPerson().getPhone().equals(owner.getPerson().getPhone()) &&
-                        o.getPerson().getAddress().equals(owner.getPerson().getAddress())) {
-                    System.out.println(o.getPerson().getPersonName() + " " + o.getPerson().getLastName()
-                            + " already exist as owner.(ownerId: " + o.getId() +
-                            " personId: " + o.getPerson().getId() + ")");
-                    return;
-                }
-            }
-        }
-
-        if (!exists) {
-            // if person does not exist create a new person in the database
+        if (personService.getById(owner.getPerson().getId()) == null) {
             personService.insert(owner.getPerson());
         }
-        // Insert owner in the Database
         ownerDao.insert(owner);
     }
 
